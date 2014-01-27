8cee22f77bc001758167985886d1b34fa099885e
bratzie <xumbik@gmail.com>
2014-01-27 18:29:46 +0100

    Forgot a file. I suck...

---
+++ minichat/src/ServerThread.java
@@ -0,0 +1,43 @@
+/**
+ * Created by bratzie on 27/01/14.
+ */
+
+import java.io.*;
+import java.net.*;
+
+public class ServerThread extends Thread {
+    private Server server;
+    private Socket socket;
+    private BufferedReader in;
+    private PrintStream out;
+
+    public ServerThread (Server server, Socket socket) throws Exception {
+        super();
+        this.server = server;
+        this.socket = socket;
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintStream(socket.getOutputStream());
+    }
+
+    public void send(String message) {
+        try {
+            out.println(message);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void run() {
+        // initialize string to avoid sending null value.
+        String message = "";
+        while(true) {
+            try {
+                message = in.readLine();
+                server.sendToAll(message);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
